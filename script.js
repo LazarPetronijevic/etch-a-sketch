7dced93094e9ee5b474f84f0f22f0c267cf15dc9
Lazar Petronijevic <>
2023-12-02 14:58:44 +0100

    color input is now done where you can select which color you want

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let size = document.getElementById("size")
 let container = document.querySelector(".container")
+let color = document.getElementById("color")
+let buttons = document.querySelector(".buttons")
+let submit = document.getElementById("submit")
+let message_container = document.getElementById("message_container")
+let color_value;
 
 size.addEventListener("click", () =>{
     let choice = Number(prompt("Enter here"))
@@ -10,11 +15,25 @@ size.addEventListener("click", () =>{
             grid_div.style.cssText += `width:${500/choice}px`
             grid_div.style.cssText += `height:${500/choice}px`
             container.appendChild(grid_div)
+
+            grid_div.addEventListener("mouseover", () => {
+                grid_div.style.backgroundColor = color_value
+            })
         }
     }
     else{
-        let message = document.createElement("div")
+        let message = document.createElement("p")
         message.textContent = "You entered invalid number"
-        container.appendChild(message)
+        message_container.appendChild(message)
     }
+})
+
+let color_input = document.createElement("input")
+color.addEventListener("click", () =>{
+    color_input.setAttribute("type", "color")
+    buttons.insertBefore(color_input,submit)
+})
+
+submit.addEventListener("click", () =>{
+    color_value = color_input.value
 })
